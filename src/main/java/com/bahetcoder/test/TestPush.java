f5b7ed1fc398ab5b36c9e3a241aa1c60696bef73
peipei2333 <55234026+peipei2333@users.noreply.github.com>
2022-09-17 22:06:48 +0800

    测试提交3个文件git分支变化

---
+++ src/main/java/com/bahetcoder/test/TestPush.java
@@ -0,0 +1,11 @@
+package com.bahetcoder.test;
+
+/**
+ * @author peipei
+ * @create 2022-09-17 21:53
+ * @discription 对类的描述
+ */
+public class TestPush {
+    //代码修改注释，用来测试git的操作
+
+}
---
+++ src/main/java/com/bahetcoder/test/TestPush3.java
@@ -0,0 +1,9 @@
+package com.bahetcoder.test;
+
+/**
+ * @author peipei
+ * @create 2022-09-17 22:06
+ * @discription 对类的描述
+ */
+public class TestPush3 {
+}
