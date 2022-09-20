08b87b1dd695ef9058f6850c47c4144cbcea964e
peipei2333 <55234026+peipei2333@users.noreply.github.com>
2022-09-20 22:53:28 +0800

    添加MasterBranchTest3文件到远程仓库中

---
+++ src/main/java/com/bahetcoder/test/MasterBranchTest3.java
@@ -0,0 +1,13 @@
+package com.bahetcoder.test;
+
+/**
+ * @author peipei
+ * @create 2022-09-20 22:52
+ * @discription 对类的描述
+ */
+public class MasterBranchTest3 {
+    public static void main(String[] args) {
+        //又添加一个文件纳入分支管理中
+        System.out.println("哈哈哈");
+    }
+}
