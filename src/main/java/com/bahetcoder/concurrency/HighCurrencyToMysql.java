f96e297a6b8287f90130d2a972fbe9b69da90041
peipei2333 <55234026+peipei2333@users.noreply.github.com>
2022-09-21 22:17:29 +0800

    对数据库进行高可用验证，用压力测试进行，主要针对select *语句操作

---
+++ src/main/java/com/bahetcoder/concurrency/HighCurrencyToMysql.java
@@ -0,0 +1,13 @@
+package com.bahetcoder.concurrency;
+
+/**
+ * @author peipei
+ * @create 2022-09-21 22:12
+ * @discription 对mysql进行海量数据写入和读的操作，最重要的是读，即select*
+ */
+public class HighCurrencyToMysql {
+    //预研，看目前有什么办法对mysql进行压力测试的
+    public static void main(String[] args) {
+        System.out.println("一定要成功，一定得让项目落地才行");
+    }
+}
