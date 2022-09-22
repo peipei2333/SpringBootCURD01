ed0bfb2f716aa5576960d9b8a03574b1398329dd
peipei2333 <55234026+peipei2333@users.noreply.github.com>
2022-09-22 22:38:02 +0800

    向mysql中插入大量的数据

---
+++ src/main/java/com/bahetcoder/bigdata/TestBigData.java
@@ -0,0 +1,12 @@
+package com.bahetcoder.bigdata;
+
+/**
+ * @author peipei
+ * @create 2022-09-22 22:36
+ * @discription 对类的描述
+ */
+public class TestBigData {
+    public static void main(String[] args) {
+        System.out.println("向mysql中插入大量的数据");
+    }
+}
